74fa0ef79e3fe023274d490803ca89938d6c89d2
frreiss <frreiss@us.ibm.com>
2020-07-02 11:32:59 -0700

    Make the table of span offsets optional

--- text_extensions_for_pandas/test_util.py
+++ text_extensions_for_pandas/test_util.py
@@ -33,17 +33,13 @@ _TEST_TOKS = make_tokens_and_features(_TEST_TEXT, _SPACY_LANGUAGE_MODEL)
 class UtilTest(TestBase):
     def test_pretty_print_html(self):
         self.maxDiff = None
-        html = pretty_print_html(_TEST_TOKS["token_span"].values)
+        html = pretty_print_html(_TEST_TOKS["token_span"].values, True)
         suffix = html[-809:]
         # print(f"[[[{suffix}]]]")
         self.assertEqual(
             suffix,
             """\
-</td>
-    </tr>
-    <tr>
-      <th>7</th>
-      <td>20</td>
+    <td>20</td>
       <td>26</td>
       <td>7</td>
       <td>8</td>
@@ -51,7 +47,25 @@ class UtilTest(TestBase):
     </tr>
   </tbody>
 </table>
+            </div>
+            <div id="text"
+             style="float:right; background-color:#F5F5F5; border: 1px solid #E0E0E0; width: 60%;">
+                <div style="float:center; padding:10px">
+                    <p style="font-family:monospace">
+                        <span style="background-color:yellow">Item&#39;s</span> <span style="background-color:yellow">for</span> <span style="background-color:yellow">&lt;</span> <span style="background-color:yellow"><span>&#36;</span>100</span> <span style="background-color:yellow">&amp;</span> <span style="background-color:yellow">change
+                    </p>
+                </div>
+            </div>
         </div>
+        """)
+
+        html = pretty_print_html(_TEST_TOKS["token_span"].values, False)
+        suffix = html[-809:]
+        # print(f"[[[{suffix}]]]")
+        self.assertEqual(
+            suffix,
+            """\
+
         <div id="text"
          style="float:right; background-color:#F5F5F5; border: 1px solid #E0E0E0; width: 60%;">
             <div style="float:center; padding:10px">
@@ -60,7 +74,4 @@ class UtilTest(TestBase):
                 </p>
             </div>
         </div>
-    </div>
-    """,
-        )
-
+        """)
