68eaeb957e175ba3901f95b11a96160c69ee2192
frreiss <frreiss@us.ibm.com>
2020-07-07 18:25:57 -0700

    Add copy constructor and test of sum aggregates

--- text_extensions_for_pandas/array/tensor.py
+++ text_extensions_for_pandas/array/tensor.py
@@ -103,7 +103,7 @@ class TensorArray(pd.api.extensions.ExtensionArray, TensorOpsMixin):
     Each tensor must have the same shape.
     """
 
-    def __init__(self, values: Union[np.ndarray, Sequence[np.ndarray]],
+    def __init__(self, values: Union[np.ndarray, Sequence[np.ndarray], "TensorArray"],
                  make_contiguous: bool = True):
         """
         :param values: A `numpy.ndarray` or sequence of `numpy.ndarray`s of equal shape.
@@ -113,6 +113,8 @@ class TensorArray(pd.api.extensions.ExtensionArray, TensorOpsMixin):
             self._tensor = values
         elif isinstance(values, Sequence):
             self._tensor = np.stack(values, axis=0) if len(values) > 0 else np.array([])
+        elif isinstance(values, TensorArray):
+            self._tensor = values._tensor.copy()
         else:
             raise TypeError(f"Expected a numpy.ndarray or sequence of numpy.ndarray, "
                             f"but received {values} "
