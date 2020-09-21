eac46bcb7f2561ea09ba3bd1c553108d1a0e30fa
frreiss <frreiss@us.ibm.com>
2020-09-21 13:23:39 -0700

    Prepping for Github Pages

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,18 +4,20 @@
 # Generate API documentation for the Text_extensions for pandas package
 # using Sphinx.
 # 
-# To make adjustments to the landing page, edit the index.rst file in the docs directory 
-# To make adjustments to the style or change settings, modify the conf.py in the docs directory 
+# To make adjustments to the landing page, edit the index.rst file in the 
+# api_docs directory 
+# To make adjustments to the style or change settings, modify the conf.py in the 
+# api_docs directory 
 # 
 # This script is made to run using the same python environment as is used for the package
 # and should be run accordingly 
 
 # Clean up the results of previous runs
-rm -rf docs/_build/html
+rm -rf api_docs/_build/html
 
 # Old code to remove the outputs of Sphinx autoapi; no longer needed
-#find docs -not -name 'index.rst' -name '*.rst' -delete
+#find api_docs -not -name 'index.rst' -name '*.rst' -delete
 
 # Invoke Sphinx
-sphinx-build -b html docs docs/_build/html
+sphinx-build -b html api_docs api_docs/_build/html
 
