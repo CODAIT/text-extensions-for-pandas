53cc009e7def92db0a16acda2ac6558b4c183755
frreiss <frreiss@us.ibm.com>
2020-09-28 17:26:36 -0700

    Add missing import

--- binder/download_bert.py
+++ binder/download_bert.py
@@ -4,6 +4,8 @@
 # Post-build script to download and cache a BERT model so that
 # Model_Training_with_BERT.ipynb won't run of disk space.
 
+import transformers
+
 bert_model_name = "dslim/bert-base-NER"
 bert = transformers.BertModel.from_pretrained(bert_model_name)
 
