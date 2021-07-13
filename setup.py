69d75c912826824af5a302b05f1ee91dd1a77bb4
frreiss <frreiss@us.ibm.com>
2021-07-13 10:18:50 -0700

    Increment version number and fix some typos

--- setup.py
+++ setup.py
@@ -24,13 +24,12 @@ with open('requirements.txt') as fh:
 
 setuptools.setup(
     name="text_extensions_for_pandas",
-    version="0.1b4",
+    version="0.1",
     author="IBM",
-    author_email="frreiss@example.com",
+    author_email="frreiss@us.ibm.com",
     description="Natural language processing support for Pandas dataframes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # Note that this URL is where the project *will* be, not where it currently is.
     url="https://github.com/CODAIT/text-extensions-for-pandas",
     install_requires=requirements,
     packages=setuptools.find_packages(),
