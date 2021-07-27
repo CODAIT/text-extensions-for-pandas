b2a36720518283f6efcc1f033026b9904d3a2404
Cole Potter <decerez@gmail.com>
2021-07-27 11:23:12 -0500

    Added start of table component

--- text_extensions_for_pandas/jupyter/widget.py
+++ text_extensions_for_pandas/jupyter/widget.py
@@ -22,6 +22,7 @@
 #
 
 import idom
+import ipywidgets as ipw
 
 def render(dataframe):
     
@@ -32,9 +33,44 @@ def render(dataframe):
         "dataframe": dataframe
     })
 
-@idom.component
 def DataFrameWidget(props):
     """The base component of the dataframe widget"""
-    return idom.html.div(
-        "Placeholder"
-    )
+    return DataFrameTableComponent(props)
+
+def DataFrameTableComponent(props):
+    """Component representing the complete table of a dataframe widget."""
+    dataframe = props["dataframe"]
+    # For each row in the dataframe, create a table with that data
+    table_headers = []
+    for column in dataframe.columns:
+        table_headers.append(f"<th>{column}</th>")
+
+    table_rows = []
+    for df_index, df_row in props["dataframe"].iterrows():
+        table_rows.append(DataFrameTableRowComponent({"df_index": df_index, "df_row": df_row, "columns": dataframe.columns}))
+    
+    table_html = f"""
+        <table>
+            <tr>
+                {"".join(table_headers)}
+            </tr>
+            {"".join(table_rows)}
+        </table>
+    """
+    return ipw.HTML(table_html)
+
+def DataFrameTableRowComponent(props):
+    """Responsible for returning the HTML representation of the single row defined in the props."""
+    row = props["df_row"] # Row of the dataframe
+    columns = props["columns"] # Index of dataframe columns
+    # For each column, create a new table header
+    table_html_pieces = []
+    for column in columns:
+        table_html_pieces.append(f"""
+            <td data-column={column}>{row[column]}</td>
+        """)
+    return f"""
+    <tr>
+        {"".join(table_html_pieces)}
+    </tr>
+    """
