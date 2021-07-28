aedf5a3cfa2e2a8273e75d694eb386c63bf3df2a
Cole Potter <decerez@gmail.com>
2021-07-28 13:14:49 -0500

    Added backend sync and temporary widget output

--- text_extensions_for_pandas/jupyter/widget.py
+++ text_extensions_for_pandas/jupyter/widget.py
@@ -25,7 +25,7 @@ import idom
 import pandas
 import ipywidgets as ipw
 from pandas.core.frame import DataFrame
-from traitlets import HasTraits, List, Dict
+from traitlets import HasTraits, Dict, Int, Bool, All, default, observe
 
 
 def render(dataframe):
@@ -36,28 +36,59 @@ def render(dataframe):
     return DataFrameWidget({
         "dataframe": dataframe
     })
+
+class MetaData(HasTraits):
+    selected = Bool().tag(sync=True)
+    output = None
+
+    @observe('selected')
+    def _selected_changed(self, change):
+        with self.output:
+            print(change)
+
+    def __init__(self, df_index, output):
+        self.output = output
+    
+    def __repr__(self):
+        return f"{self.selected}"
 class DataFrameWidget(HasTraits):
     _dataframe = Dict()
     widget = None
+    widget_output = ipw.Output()
 
     def __init__(self, props):
         dataframe = props["dataframe"]
         self._dataframe = dataframe.to_dict("split")
         self._dataframe["columns"].insert(0, "metadata")
         for row_index in range(len(self._dataframe["data"])):
-            self._dataframe["data"][row_index].insert(0, 0)
+            row_metadata = MetaData(row_index, self.widget_output)
+            row_metadata.selected = True
+            self._dataframe["data"][row_index].insert(0, row_metadata)
         self.widget = DataFrameWidgetComponent({
-            "dataframe": self._dataframe
+            "dataframe": self._dataframe,
+            "update_metadata": self.update_metadata
         })
+        self.widget.observe(self.print_change, names=All)
 
     def display(self):
-        return self.widget
-        
-    def _repr_html_(self):
-        return "Call this object's display method to view the widget."
+        self.print_change("Ready")
+        return ipw.VBox([self.widget_output, self.widget])
     
     def to_dataframe(self):
+        # df = self._dataframe.copy(deep=True)
+        # for row_index in range(len(self._dataframe["data"])):
+        #     df["data"][row_index][0] = self._dataframe["data"][row_index][0].selected   
         return pandas.DataFrame.from_records(self._dataframe["data"], index=self._dataframe["index"], columns=self._dataframe["columns"])
+
+    def print_change(self, change):
+        with self.widget_output:
+            print(change)
+    
+    def update_metadata(self, change):
+        index = int(change['owner']._dom_classes[0])
+        self._dataframe["data"][index][0].selected = change["new"]
+        with self.widget_output:
+            print(f"{index}: {change}")
 
 def DataFrameWidgetComponent(props):
     """The base component of the dataframe widget"""
@@ -66,23 +97,31 @@ def DataFrameWidgetComponent(props):
 def DataFrameTableComponent(props):
     """Component representing the complete table of a dataframe widget."""
     dataframe = props["dataframe"]
+    update_metadata = props["update_metadata"]
     # For each row in the dataframe, create a table with that data
 
     table_rows = []
     for df_index in range(len(dataframe["data"])):
         df_row = dataframe["data"][df_index]
-        table_rows.append(DataFrameTableRowComponent({"df_index": df_index, "df_row": df_row, "columns": dataframe["columns"]}))
+        table_rows.append(DataFrameTableRowComponent({"df_index": df_index, "df_row": df_row, "columns": dataframe["columns"], "update_metadata": update_metadata}))
     
     return ipw.VBox(table_rows)
 
 def DataFrameTableRowComponent(props):
     """Responsible for returning the HTML representation of the single row defined in the props."""
     row = props["df_row"] # Row of the dataframe
+    df_index = props["df_index"]
     columns = props["columns"] # Index of dataframe columns
+    update_metadata = props["update_metadata"]
+    # Create the metadata controls
+    selected_cbox = ipw.Checkbox(value=row[0].selected, indent=False)
+    selected_cbox.add_class(str(df_index))
+    selected_cbox.observe(update_metadata, names='value')
+
     # For each column, create a new table header
     table_row_cells = []
     table_row_cells.append(
-        ipw.Box(children=[ipw.Checkbox(value=row[0])], layout=ipw.Layout(flex='0 0 auto'))
+        ipw.Box(children=[selected_cbox], layout=ipw.Layout(flex='0 0 auto'))
     )
     for column_index in range(1, len(columns)):
         table_row_cells.append(
